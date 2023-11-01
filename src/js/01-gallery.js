611befe36aea6df2ccd7f0877a3c066c06b08421
frirw <jasperbmg@gmail.com>
2023-11-01 13:39:21 +0200

    main

---
+++ src/js/01-gallery.js
@@ -0,0 +1,8 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import throttle from 'lodash.throttle';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
