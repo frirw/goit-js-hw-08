7959fc5a5d2a06cabf4c59f38b0b34a2d0df5936
frirw <jasperbmg@gmail.com>
2023-11-01 21:53:34 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
-const player = new Player(iframe); 
+const player = new Player(iframe);
 
+player.on(
+  'timeupdate',
+  throttle(function () {
+    player.getCurrentTime().then(function (seconds) {
+      console.log(seconds);
+      localStorage.setItem('videoplayer-current-time', seconds);
+    });
+  }, 1000)
+);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(savedTime || 0);
