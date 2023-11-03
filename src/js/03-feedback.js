8aad66fe242e459944ecb77c46a86ea86fd8bde4
frirw <jasperbmg@gmail.com>
2023-11-03 13:07:38 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,46 @@
 import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const email = feedbackForm.querySelector('input[name="email"]');
+const message = feedbackForm.querySelector('textarea[name="message"]');
+
+const storageKey = 'feedback-form-state';
+
+feedbackForm.addEventListener('submit', onSubmitHandle);
+feedbackForm.addEventListener('input', throttle(inputHandle, 500));
+
+savedStorageValue();
+
+function onSubmitHandle(event) {
+  event.preventDefault();
+  if (email.value === '' || message.value === '') {
+    return alert('Всі поля повинні бути заповнені.');
+  }
+  const inputData = {
+    email: email.value,
+    message: message.value,
+  };
+  console.log(inputData);
+  event.currentTarget.reset();
+  localStorage.removeItem(storageKey);
+}
+
+function inputHandle(event) {
+  const inputData = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem(storageKey, JSON.stringify(inputData));
+}
+
+function savedStorageValue() {
+  const savedInput = JSON.parse(localStorage.getItem(storageKey));
+  if (savedInput) {
+    if (savedInput.message) {
+      message.value = savedInput.message;
+    }
+    if (savedInput.email) {
+      email.value = savedInput.email;
+    }
+  }
+}
